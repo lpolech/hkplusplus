a373885a0a3a71b68646dc09597bc9586e72c834
JakubC <JakubC@DESKTOP-JEIPLD2>
2019-12-05 12:21:17 +0100

    Moved tests to new package

--- test/test/center/metod/TestCentroid.java
+++ test/test/center/metod/TestCentroid.java
@@ -1,6 +1,6 @@
 package test.center.metod;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Paths;
 
@@ -21,35 +21,34 @@ public class TestCentroid {
 	Data inputData;
 	Centroid centroid;
 	Cluster cluster;
-	
-	public TestCentroid()
-	{
-		centroid= new Centroid();
+
+	public TestCentroid() {
+		centroid = new Centroid();
 		Parameters.setVerbose(true);
 		Parameters.setClassAttribute(true);
-		inputData = DataReader.read(Paths.get("test4.csv"));
+		inputData = DataReader.read(Paths.get("test.csv"));
 	}
-	
+
 	@Test
 	public void testMakeCluster() {
 		cluster = centroid.makeCluster(inputData, new L2Norm());
-		assertEquals(0.5, cluster.getCenter().getCoordinate(0),Parameters.getEpsilon());
-		assertEquals(0.5, cluster.getCenter().getCoordinate(1),Parameters.getEpsilon());
+		assertEquals(0.5, cluster.getCenter().getCoordinate(0), Parameters.getEpsilon());
+		assertEquals(0.5, cluster.getCenter().getCoordinate(1), Parameters.getEpsilon());
 		cluster.getCenter();
-		assertEquals(2, DataPoint.getNumberOfDimensions());
+		assertEquals(3, DataPoint.getNumberOfDimensions());
 	}
 
 	@Test
 	public void testUpdateCenter() {
 		Cluster cluster = centroid.makeCluster(inputData, new L2Norm());
-		Cluster cluster2= centroid.updateCenter(cluster, new  L2Norm());
+		Cluster cluster2 = centroid.updateCenter(cluster, new L2Norm());
 		assertEquals(cluster2, cluster);
 	}
-	
-	@Test(expected = NotImplementedException.class) 
-	public void testUpdateCenterThrowsException()  {
+
+	@Test(expected = NotImplementedException.class)
+	public void testUpdateCenterThrowsException() {
 		Cluster cluster = centroid.makeCluster(inputData, new GMMBayesMLE());
-		Cluster cluster2= centroid.updateCenter(cluster, new  GMMBayesMLE());
+		Cluster cluster2 = centroid.updateCenter(cluster, new GMMBayesMLE());
 		assertEquals(cluster2, cluster);
 	}
 
---
+++ test/test/data/TestDataReader.java
@@ -0,0 +1,40 @@
+package test.data;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import data.Data;
+import data.DataReader;
+import data.Parameters;
+
+public class TestDataReader {
+
+	@Test
+	public void testRead() {
+		Parameters.setVerbose(true);
+		Parameters.setClassAttribute(true);
+		// Parameters.setInstanceName(true);
+		Data inputData = DataReader.read(Paths.get("test.csv"));
+
+		System.out.println(inputData.getPoints()[0]);
+		System.out.println(inputData.getPoints()[1]);
+		System.out.println(inputData.getPoints()[2]);
+		System.out.println(inputData.getPoints()[9]);
+
+		assertEquals(10, inputData.getPoints().length);
+		assertEquals(0, inputData.getPoints()[0].getSourceCoordinate(0), Parameters.getEpsilon());
+		assertEquals(0, inputData.getPoints()[0].getSourceCoordinate(1), Parameters.getEpsilon());
+		assertEquals(1, inputData.getPoints()[9].getSourceCoordinate(1), Parameters.getEpsilon());
+		assertEquals(1, inputData.getPoints()[9].getSourceCoordinate(0), Parameters.getEpsilon());
+
+		assertEquals(0, inputData.getPoints()[1].getCoordinate(0), Parameters.getEpsilon());
+		assertEquals(0, inputData.getPoints()[1].getCoordinate(1), Parameters.getEpsilon());
+		assertEquals(1, inputData.getPoints()[2].getCoordinate(0), Parameters.getEpsilon());
+		assertEquals(1, inputData.getPoints()[2].getCoordinate(1), Parameters.getEpsilon());
+
+	}
+
+}
